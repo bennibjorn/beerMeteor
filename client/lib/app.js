29c14ad1e2993d8b4ef534941f57d1808799da39
bennibjorn <bennibjorn@hotmail.com>
2015-09-26 22:46:51 +0000

    developing on android platform

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,1 +1,10 @@
 angular.module('beerMeteor', ['angular-meteor', 'ui.router', 'chart.js']);
+
+function onReady() {
+  angular.bootstrap(document, ['beerMeteor']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
