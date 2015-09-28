0bef6b84089062323fd8ffe252b9bb6b4ec75791
bennibjorn <bennibjorn@hotmail.com>
2015-09-28 02:14:15 +0000

    cleanup and create new button

--- client/home/controllers/homeController.js
+++ client/home/controllers/homeController.js
@@ -1,16 +1,11 @@
-angular.module('beerMeteor').controller('HomeController', ['$scope', '$meteor', function ($scope, $meteor) {
+angular.module('beerMeteor').controller('HomeController', ['$scope', '$meteor',
+    function ($scope, $meteor) {
     
-    $scope.events = [
-        {
-            "id": 0,
-            "name": "bjorsmokkun benna",
-            "owner": "bennibjorn@hotmail.com",
-            "public": true
-        }
-    ];
+    $scope.events = $meteor.collection(Events);
 
-    
-
+    $scope.createEvent = function() {
+        // modal create new event
+    }
 
     var init = function() {
         console.log("init");
