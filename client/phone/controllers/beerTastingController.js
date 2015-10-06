33af8c35052a30d2f38035690876a8699f2111c1
bennibjorn <bennibjorn@hotmail.com>
2015-10-06 14:16:29 +0000

    lookin' good, loading appropriate amount of beers for event

--- client/phone/controllers/beerTastingController.js
+++ client/phone/controllers/beerTastingController.js
@@ -1,15 +1,15 @@
 angular.module('beerMeteor').controller('BeerTastingController', ['$scope', '$meteor', '$stateParams',
         function ($scope, $meteor, $stateParams) {
 
-            $scope.eventID = $meteor.object(Events, $stateParams.id);
+    $scope.eventObj = $meteor.object(Events, $stateParams.id);
+    $scope.beerRatings = $meteor.object(BeerRating, $stateParams.id);
 
-
-    var beerNum = 0;
+    var beerNum = 1;
     $scope.tasteGrade = 0;
     $scope.smellGrade = 0;
     $scope.finishGrade = 0;
     // beers sorted by best to worst
-    $scope.beerList = $meteor.collection(Beers);
+    $scope.beerList = $scope.eventObj.beerList;
     //$scope.beerList = {};
     // labels and data for beer rating history chart
     $scope.labels = [];
@@ -18,17 +18,14 @@ angular.module('beerMeteor').controller('BeerTastingController', ['$scope', '$me
         []
     ];
 
-    $scope.submit = function(taste, smell, finish) {
-      // socket.emit send in grades
-
-      // update list of beers and sort
-      addBeer(taste, smell, finish);
-    }
-
     // adds the rating of the beer to the list
     $scope.addBeer = function(taste, smell, finish) {
-        if (taste == 0 || smell == 0 || finish == 0) {
+        if (taste === 0 || smell === 0 || finish === 0) {
             console.log("taste, smell and/or finish is not set");
+            return;
+        }
+        if (beerNum - 1 >= $scope.beerList.length) {
+            console.log("already rated all beers");
             return;
         }
       var rating = (taste + smell + finish) / 3;
@@ -40,42 +37,39 @@ angular.module('beerMeteor').controller('BeerTastingController', ['$scope', '$me
           'rating': rating
       };
       console.log("Pushing beer to beerList, beerNum: " + beerNum + ", taste: " + taste + ", smell: " + smell + ", finish: " + finish + ", rating: " + rating);
-      $scope.beerList.save(beerGrade);
+      //$scope.beerList.save(beerGrade);
+      $scope.data[0][beerNum-1] = rating;
       beerNum++;
         // disable button until next beer/round starts
-    }
+    };
     // removeAll for testing purposes
     window.removeAll = function() {
       $scope.beerList.remove();
-    }
+    };
     // to sort the beers from best to worst,
     //should be called after all beers have been rated for results
     var sortBeers = function() {
       console.log("sortBeers called");
       //var listLength = $scope.beerList.size();
       //console.log("listLength: " + listLength);
-    }
+    };
     var updateChart = function() {
         // get amount of beers
         // avoid duplicates, might have to clear data before this
-        for (var i = 0; i < 4; i++) {
-            console.log("pushing beerNum: " + $scope.beerList[i].beerNum + ", and rating: " + $scope.beerList[i].rating);
+        for (var i = 0; i < $scope.beerList.length; i++) {
+            console.log("pushing beerNum: " + $scope.beerList[i].beerNum + ", and rating: " + 0);
             $scope.labels.push($scope.beerList[i].beerNum);
-            if ($scope.beerList[i] == null) {
-                //rating not yet given set as 0
-                $scope.data[0].push(0);
-            } else {
-                $scope.data[0].push($scope.beerList[i].rating);
-            }
+            $scope.data[0].push(0);
         }
-    }
 
-    // socket.on('beerNum') - sets the number of the beer
+    };
 
     var init = function() {
         console.log("init");
-        //console.log("beerList.size() = " + $scope.beerList.size());
+        console.log("eventObj: ");
+        console.log($scope.eventObj);
+        console.log("beerList.length = " + $scope.beerList.length);
         updateChart();
-    }
+    };
     init();
 }]);
